0871496900a88dc07ece38c9a0bcf3db909dbc55
Jake Schilling <jake@schillingcentral.net>
2023-08-13 16:27:53 -0600

    Allow CORS

--- src/main/java/com/portfolio/clack/security/WebSecurityConfig.java
+++ src/main/java/com/portfolio/clack/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -64,6 +65,7 @@ public class WebSecurityConfig {
             );
 
     http.authenticationProvider(authenticationProvider());
+    http.cors(Customizer.withDefaults());
 
     http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
